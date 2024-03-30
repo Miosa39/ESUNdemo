58c294716314ef04800d07977cb9713b2c47957c
Miosa <wsxwsx25893381@gmail.com>
2024-03-30 10:53:59 +0800

    0.0.1

---
+++ src/test/java/org/example/esundemo/EsuNdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.esundemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsuNdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
